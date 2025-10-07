dcd6bf3c2824297e898a8408c2fd9760fbd4b200
josef-singer <josef.singer@htlpinkafeld.at>
2025-10-07 09:08:47 +0200

    feat: add ground humidity sensor

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,19 @@
 #include "Arduino.h"
 #include "DHT.h"
+
 #define DHTPIN 26 
 #define DHTTYPE DHT11
+
 DHT dht(DHTPIN, DHTTYPE);
+
+const int digitalPinBoden = 14;
+const int analogPinBoden = 35;
 
 void setup() {
 Serial.begin(115200);
 Serial.println(F("DHTxx test!"));
 dht.begin();
+pinMode(digitalPinBoden, INPUT);
 }
 void loop() {
   // Wait a few seconds between measurements.
@@ -27,6 +33,20 @@ void loop() {
   Serial.print(F("% \nTemperature: "));
   Serial.print(t);
   Serial.print(F("°C "));
+
+  int analogValBoden = analogRead(analogPinBoden);
+  int digitalValBoden = digitalRead(digitalPinBoden);
+
+  float moisturePercent = map(analogValBoden, 0, 4095, 100, 0);
+
+  Serial.print("\n\nAnalog: ");
+  Serial.print(analogValBoden);
+  Serial.print(" Feuchte ≈ ");
+  Serial.print(moisturePercent);
+  Serial.print("%  \nDigital: ");
+  Serial.println(digitalValBoden);
+
+  delay(1000);
 
   return;
 }
