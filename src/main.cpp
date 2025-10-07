ea3a1ee84ac449b8b0883d215a63a8a1045e2a9b
josef-singer <josef.singer@htlpinkafeld.at>
2025-10-07 08:35:51 +0200

    feat: set up temperature and humidity sensor

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,32 @@
-#include <Arduino.h>
-#include <OneWire.h>
-#include <DallasTemperature.h>
-
-// Pin, an dem der Datenpin (DQ / OUT) angeschlossen ist
-#define ONE_WIRE_BUS 14
-
-// OneWire-Instanz erzeugen
-OneWire oneWire(ONE_WIRE_BUS);
-
-// DallasTemperature-Bibliothek mit der OneWire-Instanz verbinden
-DallasTemperature sensors(&oneWire);
+#include "Arduino.h"
+#include "DHT.h"
+#define DHTPIN 26 
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
-  Serial.begin(115200);
-  sensors.begin();
-  Serial.println("DS18B20 Test gestartet");
+Serial.begin(115200);
+Serial.println(F("DHTxx test!"));
+dht.begin();
 }
-
 void loop() {
-  // Temperaturmessung anfordern (alle Sensoren am Bus)
-  sensors.requestTemperatures();
-
-  // Temperatur des ersten (und einzigen) Sensors lesen
-  float tempC = sensors.getTempCByIndex(0);
-
-  if (tempC == DEVICE_DISCONNECTED_C) {
-    Serial.println("Fehler: Sensor nicht verbunden");
-  } else {
-    Serial.print("Temperatur: ");
-    Serial.print(tempC);
-    Serial.println(" °C");
+  // Wait a few seconds between measurements.
+  delay(2000);
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t)) {
+  Serial.println(F("Failed to read from DHT sensor!"));
   }
 
-  delay(1000);
+  Serial.print(F("\n\nHumidity: "));
+  Serial.print(h);
+  Serial.print(F("% \nTemperature: "));
+  Serial.print(t);
+  Serial.print(F("°C "));
+
+  return;
 }
